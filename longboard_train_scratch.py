0628df7092dace6768fbd29b19d8d4eabc07f170
Ubuntu <ubuntu@ip-172-31-1-57.us-west-2.compute.internal>
2019-08-18 22:29:47 +0000

    keras trainer

--- longboard_train_scratch.py
+++ longboard_train_scratch.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2.7
 from __future__ import print_function
 import glob, os, gc, sys
 import os.path
@@ -14,6 +13,7 @@ from IPython.display import Image as IPImage
 import shutil
 import re
 import xml.etree.ElementTree as ET
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten,Input
@@ -30,33 +30,9 @@ from keras.utils import multi_gpu_model
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, CSVLogger, EarlyStopping, TensorBoard
 from keras import Model
 from keras.utils import multi_gpu_model
-
-#config = tf.ConfigProto(log_device_placement=True)
-#config.gpu_options.allocator_type = 'BFC'
-#config.gpu_options.allow_growth = True
-#sess = tf.Session(config = config)
-#K.set_session(sess)
-
-
+from keras.models import load_model
 
 ##Path to Data
-basepath = "/home/ubuntu/efs/SLAV_Data/" 
-Bulk_1571_Cerebellum = "1571_cereb_BT_40_L3"
-Bulk_1571_Hippocampus = "1571_hippo_BT_41_L3"
-Bulk_1846_Cerebellum = "1846_cereb_BT_13_L3"
-Bulk_1846_Cortex = "1846_cortex_BT_71_L3"
-Bulk_1846_Hippocampus = "1846_hippo_BT_19_L3"
-Bulk_1846_Liver = "1846_liver_BT_22_L3"
-Bulk_5125_Cortex = "5125_cortex_BT_122_L3"
-Bulk_5125_Hippocampus = "5125_hippo_BT_139_L3"
-Bulk_5125_Liver = "5125_liver_BT_164_L3"
-
-SC_1571_Hippo = ["1571_hippo_SC_43_L3","1571_hippo_SC_45_L3","1571_hippo_SC_46_L3","1571_hippo_SC_47_L3","1571_hippo_SC_48_L3","1571_hippo_SC_50_L3","1571_hippo_SC_51_L3","1571_hippo_SC_52_L3","1571_hippo_SC_53_L3","1571_hippo_SC_55_L3","1571_hippo_SC_56_L3","1571_hippo_SC_57_L3","1571_hippo_SC_58_L3","1571_hippo_SC_59_L3","1571_hippo_SC_61_L3","1571_hippo_SC_62_L3","1571_hippo_SC_63_L3","1571_hippo_SC_64_L3"]
-SC_1846_Cortex = ["1846_cortex_SC_72_L3","1846_cortex_SC_73_L3","1846_cortex_SC_74_L3","1846_cortex_SC_75_L3","1846_cortex_SC_78_L3","1846_cortex_SC_79_L3","1846_cortex_SC_80_L3","1846_cortex_SC_81_L3","1846_cortex_SC_82_L3","1846_cortex_SC_83_L3","1846_cortex_SC_84_L3","1846_cortex_SC_85_L3","1846_cortex_SC_86_L3"]
-SC_1846_Hippo = ["1846_hippo_SC_100_L3","1846_hippo_SC_101_L3","1846_hippo_SC_102_L3","1846_hippo_SC_103_L3","1846_hippo_SC_104_L3","1846_hippo_SC_105_L3","1846_hippo_SC_106_L3","1846_hippo_SC_107_L3","1846_hippo_SC_108_L3","1846_hippo_SC_109_L3","1846_hippo_SC_110_L3","1846_hippo_SC_111_L3","1846_hippo_SC_112_L3","1846_hippo_SC_113_L3","1846_hippo_SC_88_L3","1846_hippo_SC_89_L3","1846_hippo_SC_90_L3","1846_hippo_SC_91_L3","1846_hippo_SC_92_L3","1846_hippo_SC_93_L3","1846_hippo_SC_94_L3","1846_hippo_SC_95_L3","1846_hippo_SC_99_L3"]
-SC_5125_Cortex = ["5125_cortex_SC_125_L3","5125_cortex_SC_126_L3","5125_cortex_SC_127_L3","5125_cortex_SC_128_L3","5125_cortex_SC_129_L3","5125_cortex_SC_130_L3","5125_cortex_SC_131_L3","5125_cortex_SC_132_L3","5125_cortex_SC_133_L3","5125_cortex_SC_134_L3","5125_cortex_SC_135_L3","5125_cortex_SC_136_L3","5125_cortex_SC_138_L3"]
-SC_5125_Hippo = ["5125_hippo_SC_140_L3","5125_hippo_SC_141_L3","5125_hippo_SC_142_L3","5125_hippo_SC_143_L3","5125_hippo_SC_144_L3","5125_hippo_SC_145_L3","5125_hippo_SC_147_L3","5125_hippo_SC_149_L3","5125_hippo_SC_150_L3","5125_hippo_SC_151_L3","5125_hippo_SC_152_L3","5125_hippo_SC_153_L3","5125_hippo_SC_154_L3","5125_hippo_SC_155_L3","5125_hippo_SC_156_L3","5125_hippo_SC_157_L3","5125_hippo_SC_158_L3","5125_hippo_SC_159_L3","5125_hippo_SC_160_L3","5125_hippo_SC_161_L3","5125_hippo_SC_162_L3","5125_hippo_SC_163_L3"]
-
 
 Data_Set_Train = []
 Data_Set_Validate = []
@@ -68,6 +44,9 @@ Data_Set_Validate.append([SC_5125_Hippo,Bulk_5125_Hippocampus,Bulk_5125_Liver])
 
 #TRAIN inception model on SLAV
 
+train_datagen = ImageDataGenerator(width_shift_range=0.2,height_shift_range=0.1,zoom_range=0.1,horizontal_flip=True)
+
+validate_datagen = ImageDataGenerator()
 
 img_width, img_height = 512,512
 
@@ -86,38 +65,38 @@ x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation="relu")(x)
 x = Dropout(.2)(x)
-predictions = Dense(37, activation='sigmoid')(x)
+predictions = Dense(25, activation='sigmoid')(x)
 
 # this is the model we will train
 model = Model(inputs=base_model.input, outputs=predictions)
 
-checkpoint = ModelCheckpoint("slav_incv3_scratch.h5", monitor='loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
-
+checkpoint = ModelCheckpoint("slav_incv3_scratch_25.h5", monitor='loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 early = EarlyStopping(monitor='loss', patience=10, verbose=1, mode='auto')
-
-csv_logger = CSVLogger('training_scratch.log', append=True, separator=';')
-
-tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
+csv_logger = CSVLogger('training_scratch25.log', append=True, separator=';')
+tensorboard = TensorBoard(log_dir='./logs_25', histogram_freq=0,
                           write_graph=True, write_images=False)
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-#parallel_model = multi_gpu_model(model, gpus=4)
-#parallel_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 for e in range(nb_epochs):
     print("epoch %d" % e)
     for dset in Data_Set_Train:    
         for cell in dset[0]:
             print(cell)
+            batches = 0
             if os.path.isfile(os.path.join(basepath, cell, cell+'_XY.npz')):
                 try:
                     data = np.load(os.path.join(basepath, cell, cell+'_XY.npz'))
-                    X_train = data['X'] / 255
-                    Y_train = data['Y']
+                    X = data['X']
+                    Y = data['Y']
+                    Y = Y[:,np.r_[0:21,33:37]]
+                    X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=0.2, random_state=42)
+                    data = None
+                    X = None
+                    Y = None
                     print("npz is OK")
-                    model.fit(X_train, Y_train, batch_size=batch_size, epochs=1, callbacks = [early,checkpoint,csv_logger,tensorboard], validation_split=.2, verbose = 1)
+                    model.fit_generator(train_datagen.flow(X_train, Y_train, batch_size=batch_size), callbacks = [early,checkpoint,csv_logger,tensorboard], verbose = 1, epochs=1, steps_per_epoch=len(X_train) / 32, validation_data=validate_datagen.flow(X_validate, Y_validate, batch_size=batch_size))
                 except: 
                     print("npz is bad")
                     pass
-model.save('inceptionV3_SLAV_Scratch.h5')  
+model.save('inceptionV3_SLAV_Scratch_25.h5')  
