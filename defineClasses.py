b61928c0392bcb52f67498348242c5bdd2abb40a
Ubuntu <ubuntu@ip-172-31-5-56.us-west-2.compute.internal>
2020-06-30 16:41:52 +0000

    post umap processing

--- defineClasses.py
+++ defineClasses.py
@@ -48,82 +48,59 @@ Train_C = hf['C'][()]
 Train_Y = hf['Y'][()]
 Labs = hf['L'][()]
 
+vals = np.arange(0,Train_Z.shape[0],1)
+np.random.shuffle(vals)
 
-embedding_partialsupervised = umap.UMAP(n_neighbors=25, n_components=2, min_dist=0.0).fit_transform(Train_Z, y=Train_C)
+set1 = vals[0:1592902]
+set2 = vals[1592902:3185804]
 
-hf = h5py.File('Training_partialsupervised_All.h5', 'w')
+print set1.shape
+print set2.shape
+
+Train_Z_1 = Train_Z[set1,:]
+Train_C_1 = Train_C[set1]
+
+Train_Z_2 = Train_Z[set2,:]
+Train_C_2 = Train_C[set2]
+
+print Train_Z_1.shape
+print Train_Z_2.shape
+
+print Train_C_1.shape
+print Train_C_2.shape
+
+embedding_partialsupervised_1 = umap.UMAP(n_neighbors=25, n_components=2, min_dist=0.0).fit_transform(Train_Z_1, y=Train_C_1)
+embedding_partialsupervised_2 = umap.UMAP(n_neighbors=25, n_components=2, min_dist=0.0).fit_transform(Train_Z_2, y=Train_C_2)
+
+
+hf = h5py.File('Training_UMAP_2Set.h5', 'w')
+hf.create_dataset('Y', data=Train_Y)
+hf.create_dataset('Z', data=Train_Z)
+hf.create_dataset('C', data=Train_C)##just basic classes
+hf.create_dataset('set1', data=set1)
+hf.create_dataset('set2', data=set2)
+hf.create_dataset('embedding_partialsupervised_1', data=embedding_partialsupervised_1)
+hf.create_dataset('embedding_partialsupervised_2', data=embedding_partialsupervised_2)
+hf.close()
+
+session = Session(aws_access_key_id=config[0],aws_secret_access_key=config[1])
+s3 = session.resource('s3')
+s3.meta.client.upload_file(os.path.join('Training_UMAP_2Set.h5'),'bsmn-data',os.path.join('Training_UMAP_2Set.h5'))
+
+HDB_1 = hdbscan.HDBSCAN(min_samples=14,min_cluster_size=5000,prediction_data=True).fit_predict(embedding_partialsupervised_1)
+HDB_2 = hdbscan.HDBSCAN(min_samples=14,min_cluster_size=5000,prediction_data=True).fit_predict(embedding_partialsupervised_2)
+
+session = Session(aws_access_key_id=config[0],aws_secret_access_key=config[1])
+s3 = session.resource('s3')
+hf = h5py.File('Training_UMAP_HDB_2Set.h5', 'w')
 hf.create_dataset('Y', data=Train_Y)
 hf.create_dataset('Z', data=Train_Z)
 hf.create_dataset('C', data=Train_C)
-hf.create_dataset('embedding_partialsupervised', data=embedding_partialsupervised)
-hf.close()                
-s3.meta.client.upload_file(os.path.join('Training_partialsupervised_All.h5'),'bsmn-data',os.path.join('Training_partialsupervised_All.h5'))
-
-unique, counts = np.unique(C, return_counts=True)
-print dict(zip(unique, counts))
-
-fig, ax = plt.subplots(1, figsize=(14, 10))
-plt.scatter(embedding_partialsupervised[:,0],embedding_partialsupervised[:,1], s=0.3, alpha=1)
-plt.setp(ax, xticks=[], yticks=[])
-plt.savefig('fig0.pdf') 
-
-fig, ax = plt.subplots(1, figsize=(14, 10))
-plt.scatter(embedding_partialsupervised[:,0],embedding_partialsupervised[:,1], s=0.3, c=C, cmap='nipy_spectral', alpha=1)
-plt.setp(ax, xticks=[], yticks=[])
-cbar = plt.colorbar(boundaries=np.arange(max(C)))
-cbar.set_ticks(np.arange(max(C)))
-plt.savefig('fig1.pdf') 
-
-f, ax = plt.subplots(figsize=(10, 10))
-cmap = sns.cubehelix_palette(as_cmap=True, dark=0, light=1, reverse=True)
-sns.kdeplot(embedding_partialsupervised[:,0], embedding_partialsupervised[:,1], cmap=cmap, n_levels=100, shade=True);
-plt.savefig('fig2.pdf') 
-
-HDB = hdbscan.HDBSCAN(min_samples=14,min_cluster_size=5000,prediction_data=True).fit_predict(embedding_partialsupervised)
-
-vals = np.linspace(0,1,35)
-np.random.shuffle(vals)
-cmap = plt.cm.colors.ListedColormap(plt.cm.nipy_spectral(vals))
-
-clustered = (HDB >= 0)
-fig, ax = plt.subplots(1, figsize=(14, 10))
-plt.scatter(embedding_partialsupervised[~clustered, 0],
-            embedding_partialsupervised[~clustered, 1],
-            c=(0.5, 0.5, 0.5),
-            s=0.1,
-            alpha=0)
-plt.scatter(embedding_partialsupervised[clustered, 0],
-            embedding_partialsupervised[clustered, 1],
-            c=HDB[clustered],
-            s=0.1,
-            cmap=cmap);
-cbar = plt.colorbar(boundaries=np.arange(0,max(HDB)))
-cbar.set_ticks(np.arange(0,max(HDB)))
-plt.savefig('fig3.pdf') 
-
-clustered = (HDB >= 0)
-fig, ax = plt.subplots(1, figsize=(14, 10))
-plt.scatter(embedding_partialsupervised[~clustered, 0],
-            embedding_partialsupervised[~clustered, 1],
-            c=(0.5, 0.5, 0.5),
-            s=0.1,
-            alpha=0)
-plt.scatter(embedding_partialsupervised[clustered, 0],
-            embedding_partialsupervised[clustered, 1],
-            c=C[clustered],
-            s=0.1,
-            cmap=cmap);
-cbar = plt.colorbar(boundaries=np.arange(0,max(C2[clustered])))
-cbar.set_ticks(np.arange(0,max(C[clustered])))
-plt.savefig('fig4.pdf') 
-
-session = Session(aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
-s3 = session.resource('s3') 
-hf = h5py.File('Training_UMAP_HDB.h5', 'w')
-hf.create_dataset('Y', data=Y)
-hf.create_dataset('Z', data=Z)
-hf.create_dataset('C', data=C)##just basic classes
-hf.create_dataset('HDB', data=HDB) 
-hf.create_dataset('embedding_partialsupervised', data=embedding_partialsupervised)
+hf.create_dataset('set1', data=set1)
+hf.create_dataset('set2', data=set2)
+hf.create_dataset('embedding_partialsupervised_1', data=embedding_partialsupervised_1)
+hf.create_dataset('embedding_partialsupervised_2', data=embedding_partialsupervised_2)
+hf.create_dataset('HDB_1', data=HDB_1)
+hf.create_dataset('HDB_2', data=HDB_2)
 hf.close()
-s3.meta.client.upload_file(os.path.join('Training_UMAP_HDB.h5'),'bsmn-data',os.path.join('Training_UMAP_HDB.h5'))
+s3.meta.client.upload_file(os.path.join('Training_UMAP_HDB_2Set.h5'),'bsmn-data',os.path.join('Training_UMAP_HDB_2Set.h5'))
---
+++ trainingPrep.py
@@ -0,0 +1,60 @@
+#!/home/ubuntu/miniconda2/bin/python
+
+from __future__ import print_function
+import glob, os, gc, sys
+import os.path
+import csv
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+from time import time
+from subprocess import (call, Popen, PIPE)
+from itertools import product
+from IPython.display import display
+from PIL import Image
+from IPython.display import Image as IPImage
+import shutil
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import uuid
+import pickle
+from boto3.session import Session
+import boto3
+import h5py
+
+##Path to Data
+chunk = sys.argv[1]
+basepath = "/home/ubuntu/"
+
+hf = h5py.File(os.path.join(basepath,'Training_UMAP_HDB_Final.h5'),'r')
+Train_Z = hf['Z'][()] 
+Train_C = hf['C'][()]
+Train_Y = hf['Y'][()]
+embedding_partialsupervised_1 = hf['embedding_partialsupervised_1'][()]
+embedding_partialsupervised_2 = hf['embedding_partialsupervised_2'][()]
+HDB_1 = hf['HDB_1'][()]
+HDB_2 = hf['HDB_2'][()]
+set1 = hf['set1'][()]
+set2 = hf['set2'][()]
+New_1 = hf['New_1'][()]
+New_2 = hf['New_2'][()]
+
+##Path to Data
+Training = ["USD15","usd19","USD3","USD30","USD36","USD37","USH15","USH19","USH3","USH30","USH36","USH37"]
+
+Classes1 = len(set(New_1))
+
+#pull all cells per sample from Train_Y
+L1 = len(New_1)
+T1={}
+
+l = np.array_split(np.array(range(L1)),20)
+for i in l[chunk]:
+    position_key = Train_Y[set1][i]
+    Y_Class1 = str(New_1[i])
+    T1[position_key] = Y_Class1
+    
+hf = h5py.File('Dict_'+chunk+'.h5', 'w')
+hf.create_dataset('T1', data=T1)
+hf.close()    
